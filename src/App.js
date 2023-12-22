d5ad401bb2e84b3200b3625b39332943b904f6eb
Alvaro Climent <alvarocliment1999@gmail.com>
2023-12-22 03:01:49 +0100

    Second try push Uploas App

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import MiComponente from './MiComponente';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Bienvenido al master en react
+        </p>
+        {/* Cargar mi primer componente */}
+      <MiComponente />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MiComponente.js
@@ -0,0 +1,23 @@
+// Importar modulos de react / dependencias
+
+import React from 'react';
+
+// Funcion del componente
+
+const MiComponente = () => {
+
+    return (
+    <div>
+    <hr/>
+    <h2>Componente creado</h2>
+    <p>Este es mi primer componente</p>
+    <ul>
+        <li>React</li>
+        <li>Angular</li>
+        <li>Vue</li>
+    </ul>
+    </div>
+)};
+
+// Export
+export default MiComponente;
